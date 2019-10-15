a267b9b4ff72cad5c249b40b2dae552e742b3922
marco.li@yingzt.com <marco.li@yingzt.com>
2019-10-15 16:25:23 +0800

    modify AddTwoNumbers

--- Exercise/src/main/java/com/lzl/leetcode/AddTwoNumbers.java
+++ Exercise/src/main/java/com/lzl/leetcode/AddTwoNumbers.java
@@ -49,7 +49,6 @@ public class AddTwoNumbers {
         ListNode l3 = rt;
         int carry = 0;
         while (null != l1 || null != l2 || carry != 0) {
-
             ListNode index = new ListNode(0);
 
             // 相加 并移动游标
@@ -75,6 +74,41 @@ public class AddTwoNumbers {
         }
         return rt.next;
     }
+
+    /**
+     * 将两个链表看成是相同长度的进行遍历，如果一个链表较短则在前面补 00，比如 987 + 23 = 987 + 023 = 1010
+     * 每一位计算的同时需要考虑上一位的进位问题，而当前位计算结束后同样需要更新进位值
+     * 如果两个链表全部遍历完毕后，进位值为1，则在新链表最前方添加节点1
+     * 对于链表问题，返回结果为头结点时，通常需要先初始化一个预先指针 pre，该指针的下一个节点指向真正的头结点head。使用预先指针的目的在于链表初始化时无可用节点值，而且链表构造过程需要指针移动，进而会导致头指针丢失，无法返回结果。
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers/solution/hua-jie-suan-fa-2-liang-shu-xiang-jia-by-guanpengc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while (null != l1 || null != l2) {
+
+            int x = null == l1 ? 0 : l1.val;
+            int y = null == l2 ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            cur.next = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            cur = cur.next;
+            l1 = null == l1 ? l1 : l1.next;
+            l2 = null == l2 ? l2 : l2.next;
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
 
 }
 
